2e96d1f054754313fe94044074768aca8de1626a
Advaith Menon <83835839+advaithm582@users.noreply.github.com>
2022-10-04 20:24:00 +0530

    Update demo.py

--- airport_manager/demo.py
+++ airport_manager/demo.py
@@ -5,76 +5,91 @@ import json
 import datetime
 
 import pymysql
-from pymysql.err import OperationalError
+from pymysql.err import OperationalError, Error as PYMErr, InterfaceError
 
 STYLE = dict(
     header=dict(font=("Adani Regular", 40, "bold"), fg="#ffffff", bg="#800000"),
-    body=dict(font=("Calibri", 35, "bold"), fg="#ffffff", bg="#000066")
+    body=dict(font=("Calibri", 35, "bold"), fg="#ffffff", bg="#000066"),
+    err=dict(font=("Calibri", 60, "bold"), fg="#ffffff", bg="#000066")
 )
 
 class DataFrame(tk.Frame,):
     def __init__(self, parent, con, data, style, *args, **kwargs):
         super().__init__(parent, *args, **kwargs) # initialize parent tk
-
+        
+       
         self.__con = con
         self._parent = parent
         self._data = data
         self._style = style
         
-
-        # Table adding
-        self.__heading_2 = tk.Label(self, text="Flight No", **self._style["header"])
-        self.__heading_2.grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
-        self.__heading_4 = tk.Label(self, text="To", **self._style["header"])
-        self.__heading_4.grid(row=0, column=1, sticky=tk.N+tk.S+tk.E+tk.W)
-        self.__heading_7 = tk.Label(self, text="STD", **self._style["header"])
-        self.__heading_7.grid(row=0, column=2, sticky=tk.N+tk.S+tk.E+tk.W)
-        self.__heading_8 = tk.Label(self, text="ETD", **self._style["header"])
-        self.__heading_8.grid(row=0, column=3, sticky=tk.N+tk.S+tk.E+tk.W)
-        self.__heading_9 = tk.Label(self, text="Check-in counter", **self._style["header"])
-        self.__heading_9.grid(row=0, column=4, sticky=tk.N+tk.S+tk.E+tk.W)
-        self.__heading_10 = tk.Label(self, text="Status", **self._style["header"])
-        self.__heading_10.grid(row=0, column=5, sticky=tk.N+tk.S+tk.E+tk.W)
-        
-        for i in range(6):
-            tk.Grid.columnconfigure(self, i, weight=1)
-
-
-        cur = self.__con.cursor()
-        cur.execute("SELECT `ofid`, `to`, "
-                    " `std`, `etd`, `checkinctr`, `status` "
-                    "FROM `flight` WHERE "
-                    "`std`BETWEEN NOW() AND NOW() + INTERVAL 1 DAY"
-                    " ORDER BY `std` ASC;")
-
-        i = 1
-
-        self.__matrix = []
-
-        for data in cur.fetchall():
-            l1 = tk.Label(self, text=data[0], **self._style["body"])
-            l1.grid(row=i, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
-            # print(self._data.get(data[1], data[1]), data[1])
-            l2 = tk.Label(self, text=self._data.get(data[1], data[1]), **self._style["body"])
-            l2.grid(row=i, column=1, sticky=tk.N+tk.S+tk.E+tk.W)
-
-            l3 = tk.Label(self, text=data[2].strftime("%H:%M"), **self._style["body"])
-            l3.grid(row=i, column=2, sticky=tk.N+tk.S+tk.E+tk.W)
-
-            l4 = tk.Label(self, text=data[3].strftime("%H:%M"), **self._style["body"])
-            l4.grid(row=i, column=3, sticky=tk.N+tk.S+tk.E+tk.W)
-            l5 = tk.Label(self, text=data[4], **self._style["body"])
-            l5.grid(row=i, column=4, sticky=tk.N+tk.S+tk.E+tk.W)
-
-            l6 = tk.Label(self, text=data[5].title(), **self._style["body"])
-            l6.grid(row=i, column=5, sticky=tk.N+tk.S+tk.E+tk.W)
-
-            i += 1
+        self["bg"] = self._style["body"]["bg"]
 
         
-
         
-        cur.close()
+
+        try:
+            cur = self.__con.cursor()
+            cur.execute("SELECT `ofid`, `to`, "
+                        " `std`, `etd`, `checkinctr`, `status` "
+                        "FROM `flight` WHERE "
+                        "`std`BETWEEN NOW() AND NOW() + INTERVAL 1 DAY"
+                        " ORDER BY `std` ASC;")
+        except OperationalError as e:
+            lbl = tk.Label(self, text=("Failed to update data\n"
+                                       "Sorry for inconvinience"), **self._style["body"])
+            lbl.grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
+            tk.Grid.columnconfigure(self, 0, weight=1)
+        except InterfaceError as e:
+            lbl = tk.Label(self, text=("Cannot update data\n"
+                                       "Please restart application"), **self._style["body"])
+            lbl.grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
+            rst = tk.Button(self, text="Restart", command=exit, font=("Adani Regular", 40, "bold"))
+            rst.grid(row=1, column=0, sticky=tk.N+tk.S)
+            tk.Grid.columnconfigure(self, 0, weight=1)
+        else:
+            # Table adding
+            self.__heading_2 = tk.Label(self, text="Flight No", **self._style["header"])
+            self.__heading_2.grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
+            self.__heading_4 = tk.Label(self, text="To", **self._style["header"])
+            self.__heading_4.grid(row=0, column=1, sticky=tk.N+tk.S+tk.E+tk.W)
+            self.__heading_7 = tk.Label(self, text="STD", **self._style["header"])
+            self.__heading_7.grid(row=0, column=2, sticky=tk.N+tk.S+tk.E+tk.W)
+            self.__heading_8 = tk.Label(self, text="ETD", **self._style["header"])
+            self.__heading_8.grid(row=0, column=3, sticky=tk.N+tk.S+tk.E+tk.W)
+            self.__heading_9 = tk.Label(self, text="Check-in counter", **self._style["header"])
+            self.__heading_9.grid(row=0, column=4, sticky=tk.N+tk.S+tk.E+tk.W)
+            self.__heading_10 = tk.Label(self, text="Status", **self._style["header"])
+            self.__heading_10.grid(row=0, column=5, sticky=tk.N+tk.S+tk.E+tk.W)
+            i = 1
+            
+            for i in range(6):
+                tk.Grid.columnconfigure(self, i, weight=1)
+
+            # self.__matrix = []
+
+            for data in cur.fetchall():
+                l1 = tk.Label(self, text=data[0], **self._style["body"])
+                l1.grid(row=i, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
+                # print(self._data.get(data[1], data[1]), data[1])
+                l2 = tk.Label(self, text=self._data.get(data[1], data[1]), **self._style["body"])
+                l2.grid(row=i, column=1, sticky=tk.N+tk.S+tk.E+tk.W)
+
+                l3 = tk.Label(self, text=data[2].strftime("%H:%M"), **self._style["body"])
+                l3.grid(row=i, column=2, sticky=tk.N+tk.S+tk.E+tk.W)
+
+                l4 = tk.Label(self, text=data[3].strftime("%H:%M"), **self._style["body"])
+                l4.grid(row=i, column=3, sticky=tk.N+tk.S+tk.E+tk.W)
+                l5 = tk.Label(self, text=data[4], **self._style["body"])
+                l5.grid(row=i, column=4, sticky=tk.N+tk.S+tk.E+tk.W)
+
+                l6 = tk.Label(self, text=data[5].title(), **self._style["body"])
+                l6.grid(row=i, column=5, sticky=tk.N+tk.S+tk.E+tk.W)
+
+                i += 1
+        finally:
+            cur.close()
+
 
 class HomeScreen(tk.Tk):
     def __init__(self, con, *args, **kwargs):
