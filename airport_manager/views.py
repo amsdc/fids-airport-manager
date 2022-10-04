e5b30b71d6467616a7827cdc3a3b2972ff7892f9
Advaith Menon <83835839+advaithm582@users.noreply.github.com>
2022-10-04 11:00:50 +0530

    Update views.py

--- airport_manager/views.py
+++ airport_manager/views.py
@@ -5,6 +5,56 @@ from tkinter import messagebox
 import pymysql
 from pymysql.err import OperationalError
 
+class DataFrame(tk.Frame):
+    def __init__(self, parent, con, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs) # initialize parent tk
+
+        self.__con = con
+
+        # Table adding
+        self.__heading_1 = tk.Label(self, text="Host")
+        self.__heading_1.grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
+        self.__heading_1 = tk.Label(self, text="User")
+        self.__heading_1.grid(row=0, column=1, sticky=tk.N+tk.S+tk.E+tk.W)
+        self.__heading_1 = tk.Label(self, text="Password")
+        self.__heading_1.grid(row=0, column=2, sticky=tk.N+tk.S+tk.E+tk.W)
+        self.__heading_1 = tk.Label(self, text="Button")
+        self.__heading_1.grid(row=0, column=3, sticky=tk.N+tk.S+tk.E+tk.W)
+
+        cur = self.__con.cursor()
+        cur.execute("select host, user, password, plugin from user")
+
+        i = 1
+
+        self.__matrix = []
+
+        for data in cur.fetchall():
+            l1 = tk.Label(self, text=data[0])
+            l1.grid(row=i, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
+            l2 = tk.Label(self, text=data[1])
+            l2.grid(row=i, column=1, sticky=tk.N+tk.S+tk.E+tk.W)
+
+            l3 = tk.Label(self, text=data[2])
+            l3.grid(row=i, column=2, sticky=tk.N+tk.S+tk.E+tk.W)
+
+            
+            def callback(plugin=data[3]):
+                if plugin:
+                    messagebox.showinfo("Yes", "Plugin present "+data[3])
+                else:
+                    print("no")
+            
+            b4 = ttk.Button(self, text="Show plugin", command=callback)
+            b4.grid(row=i,column=3, sticky=tk.N+tk.S+tk.E+tk.W)
+
+            i += 1
+
+        
+
+        
+        cur.close()
+
+
 
 class HomeScreen(tk.Tk):
     def __init__(self, con, *args, **kwargs):
@@ -17,14 +67,18 @@ class HomeScreen(tk.Tk):
         self.__logout_btn.grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
 
         self.__menu = tk.Menu(self)
+        
         self.__user_menu = tk.Menu(self.__menu, tearoff=0)
         self.__user_menu.add_command(label="Logout", command=self.__logout)
+        
         self.__menu.add_cascade(label="User", menu=self.__user_menu)
         
         self.config(menu=self.__menu)
 
+        self.__dframe = DataFrame(self, self.__con)
+        self.__dframe.grid(row=1, column=0)
         
-
+        '''
         # Table adding
         self.__heading_1 = tk.Label(self, text="Host")
         self.__heading_1.grid(row=1, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
@@ -43,28 +97,31 @@ class HomeScreen(tk.Tk):
         self.__matrix = []
 
         for data in cur.fetchall():
-            self.__matrix.append([])
             l1 = tk.Label(self, text=data[0])
             l1.grid(row=i, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
-            self.__matrix[-1].append(l1)
             l2 = tk.Label(self, text=data[1])
             l2.grid(row=i, column=1, sticky=tk.N+tk.S+tk.E+tk.W)
-            self.__matrix[-1].append(l2)
+
             l3 = tk.Label(self, text=data[2])
             l3.grid(row=i, column=2, sticky=tk.N+tk.S+tk.E+tk.W)
-            self.__matrix[-1].append(l3)
+
             
+            def callback(plugin=data[3]):
+                if plugin:
+                    messagebox.showinfo("Yes", "Plugin present "+data[3])
+                else:
+                    print("no")
             
-            
-            b4 = ttk.Button(self, text="Show plugin"+data[3], command=lambda: print(data[3]))
-            b4.grid(row=i,column=3)
+            b4 = ttk.Button(self, text="Show plugin", command=callback)
+            b4.grid(row=i,column=3, sticky=tk.N+tk.S+tk.E+tk.W)
+
             i += 1
 
         
 
         
         cur.close()
-        
+        '''
 
     def __logout(self):
         self.__con.close()
@@ -83,27 +140,27 @@ class LoginWindow(tk.Tk):
         self.title("User sign in")
         
         # username
-        self.username_label = ttk.Label(self, text="Username:")
-        self.username_label.grid(column = 0, row = 0, sticky = tk.W, padx = 10, pady = 10)
+        self.__username_label = tk.Label(self, text="Username:")
+        self.__username_label.grid(column = 0, row = 0, sticky = tk.W, padx = 10, pady = 10)
 
-        self.username_entry = ttk.Entry(self)
-        self.username_entry.grid(column = 1, row = 0, sticky = tk.E, padx = 10, pady = 10)
+        self.__username_entry = ttk.Entry(self)
+        self.__username_entry.grid(column = 1, row = 0, sticky = tk.E, padx = 10, pady = 10)
 
         # password
-        self.password_label = ttk.Label(self, text="Password:")
-        self.password_label.grid(column = 0, row = 1, sticky = tk.W, padx = 10, pady = 5)
+        self.__password_label = tk.Label(self, text="Password:")
+        self.__password_label.grid(column = 0, row = 1, sticky = tk.W, padx = 10, pady = 5)
 
-        self.password_entry = ttk.Entry(self, show = "*")
-        self.password_entry.grid(column = 1, row = 1, sticky = tk.E, padx = 10, pady = 5)
+        self.__password_entry = ttk.Entry(self, show = "*")
+        self.__password_entry.grid(column = 1, row = 1, sticky = tk.E, padx = 10, pady = 5)
 
         # login button
-        self.login_button = ttk.Button(self, text="Login", command=self.login)
-        self.login_button.grid(column = 1, row = 3, sticky = tk.S, padx = 5, pady = 5)
+        self.__login_button = ttk.Button(self, text="Login", command=self.__login)
+        self.__login_button.grid(column = 1, row = 3, sticky = tk.S, padx = 5, pady = 5)
 
-    def login(self):
+    def __login(self):
         try:
-            u = self.username_entry.get()
-            p = self.password_entry.get()
+            u = self.__username_entry.get()
+            p = self.__password_entry.get()
             con = pymysql.connect(host="localhost",
                                   user=u,
                                   password=p,
