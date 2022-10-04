5336a93129eb4c9972379adb101ee2e9c1a80de5
Advaith Menon <83835839+advaithm582@users.noreply.github.com>
2022-10-04 12:37:48 +0530

    Revert "Update views.py"
    This reverts commit c21057c62c51986548c22b9a4a5ea81afcabcc2c.

--- airport_manager/views.py
+++ airport_manager/views.py
@@ -14,12 +14,34 @@ class DataFrame(tk.Frame):
         # Table adding
         self.__heading_1 = tk.Label(self, text="Incoming Flight ID")
         self.__heading_1.grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
-        self.__heading_1 = tk.Label(self, text="Outgoing Flight ID")
-        self.__heading_1.grid(row=0, column=1, sticky=tk.N+tk.S+tk.E+tk.W)
-        self.__heading_1 = tk.Label(self, text="Password")
-        self.__heading_1.grid(row=0, column=2, sticky=tk.N+tk.S+tk.E+tk.W)
-        self.__heading_1 = tk.Label(self, text="Button")
-        self.__heading_1.grid(row=0, column=3, sticky=tk.N+tk.S+tk.E+tk.W)
+        self.__heading_2 = tk.Label(self, text="Outgoing Flight ID")
+        self.__heading_2.grid(row=0, column=1, sticky=tk.N+tk.S+tk.E+tk.W)
+        self.__heading_3 = tk.Label(self, text="From")
+        self.__heading_3.grid(row=0, column=2, sticky=tk.N+tk.S+tk.E+tk.W)
+        self.__heading_4 = tk.Label(self, text="To")
+        self.__heading_4.grid(row=0, column=3, sticky=tk.N+tk.S+tk.E+tk.W)
+        self.__heading_5 = tk.Label(self, text="STA")
+        self.__heading_5.grid(row=0, column=4, sticky=tk.N+tk.S+tk.E+tk.W)
+        self.__heading_6 = tk.Label(self, text="ETA")
+        self.__heading_6.grid(row=0, column=5, sticky=tk.N+tk.S+tk.E+tk.W)
+        self.__heading_7 = tk.Label(self, text="STD")
+        self.__heading_7.grid(row=0, column=6, sticky=tk.N+tk.S+tk.E+tk.W)
+        self.__heading_8 = tk.Label(self, text="ETD")
+        self.__heading_8.grid(row=0, column=7, sticky=tk.N+tk.S+tk.E+tk.W)
+        self.__heading_9 = tk.Label(self, text="Check-in counter")
+        self.__heading_9.grid(row=0, column=8, sticky=tk.N+tk.S+tk.E+tk.W)
+        self.__heading_10 = tk.Label(self, text="Status")
+        self.__heading_10.grid(row=0, column=9, sticky=tk.N+tk.S+tk.E+tk.W)
+        self.__heading_11 = tk.Label(self, text="Belt status")
+        self.__heading_11.grid(row=0, column=10, sticky=tk.N+tk.S+tk.E+tk.W)
+        self.__heading_12 = tk.Label(self, text="Gate")
+        self.__heading_12.grid(row=0, column=11, sticky=tk.N+tk.S+tk.E+tk.W)
+        self.__heading_13 = tk.Label(self, text="Belt")
+        self.__heading_13.grid(row=0, column=12, sticky=tk.N+tk.S+tk.E+tk.W)
+        self.__heading_14 = tk.Label(self, text="Edit")
+        self.__heading_14.grid(row=0, column=13, sticky=tk.N+tk.S+tk.E+tk.W)
+        self.__heading_15 = tk.Label(self, text="Delete")
+        self.__heading_15.grid(row=0, column=14, sticky=tk.N+tk.S+tk.E+tk.W)
 
         cur = self.__con.cursor()
         cur.execute("SELECT `id`, `ifid`, `ofid`, `from`, `to`, `sta`, `eta`,"
@@ -155,13 +177,6 @@ class LoginWindow(tk.Tk):
         self.__password_entry = ttk.Entry(self, show = "*")
         self.__password_entry.grid(column = 1, row = 1, sticky = tk.E, padx = 10, pady = 5)
 
-        # database
-        self.__db_label = tk.Label(self, text="Database:")
-        self.__db_label.grid(column = 0, row = 2, sticky = tk.W, padx = 10, pady = 10)
-
-        self.__db_entry = ttk.Entry(self)
-        self.__db_entry.grid(column = 1, row = 2, sticky = tk.E, padx = 10, pady = 10)
-
         # login button
         self.__login_button = ttk.Button(self, text="Login", command=self.__login)
         self.__login_button.grid(column = 1, row = 3, sticky = tk.S, padx = 5, pady = 5)
@@ -170,11 +185,10 @@ class LoginWindow(tk.Tk):
         try:
             u = self.__username_entry.get()
             p = self.__password_entry.get()
-            d = self.__db_entry.get()
             con = pymysql.connect(host="localhost",
                                   user=u,
                                   password=p,
-                                  database=d)
+                                  database="mysql")
         except OperationalError as e:
             messagebox.showerror("OperationalError", str(e))
         else:
