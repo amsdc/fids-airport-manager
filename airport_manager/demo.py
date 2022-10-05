1b140f8fbd657d4e6625b568b4f002f867488cae
Advaith Menon <83835839+advaithm582@users.noreply.github.com>
2022-10-05 06:50:27 +0530

    Update demo.py

--- airport_manager/demo.py
+++ airport_manager/demo.py
@@ -8,9 +8,12 @@ import pymysql
 from pymysql.err import OperationalError, Error as PYMErr, InterfaceError
 
 STYLE = dict(
-    header=dict(font=("Adani Regular", 40, "bold"), fg="#ffffff", bg="#800000"),
+    title=dict(font=("Calibri", 60, "bold"), fg="#ffffff", bg="#666699"),
+    time=dict(font=("Consolas", 40, "bold"), fg="#ffffff", bg="#666699"),
+    header=dict(font=("Calibri", 40, "bold"), fg="#ffffff", bg="#666699"),
     body=dict(font=("Calibri", 35, "bold"), fg="#ffffff", bg="#000066"),
-    err=dict(font=("Calibri", 60, "bold"), fg="#ffffff", bg="#000066")
+    body2=dict(font=("Calibri", 35, "bold"), fg="#ffffff", bg="#0066ff"),
+    err=dict(font=("Calibri", 60, "bold"), fg="#ff0000", bg="#000066")
 )
 
 class DataFrame(tk.Frame,):
@@ -37,12 +40,12 @@ class DataFrame(tk.Frame,):
                         " ORDER BY `std` ASC;")
         except OperationalError as e:
             lbl = tk.Label(self, text=("Failed to update data\n"
-                                       "Sorry for inconvinience"), **self._style["body"])
+                                       "Sorry for inconvinience"), **self._style["err"])
             lbl.grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
             tk.Grid.columnconfigure(self, 0, weight=1)
         except InterfaceError as e:
             lbl = tk.Label(self, text=("Cannot update data\n"
-                                       "Please restart application"), **self._style["body"])
+                                       "Please restart application"), **self._style["err"])
             lbl.grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
             rst = tk.Button(self, text="Restart", command=exit, font=("Adani Regular", 40, "bold"))
             rst.grid(row=1, column=0, sticky=tk.N+tk.S)
@@ -69,21 +72,25 @@ class DataFrame(tk.Frame,):
             # self.__matrix = []
 
             for data in cur.fetchall():
-                l1 = tk.Label(self, text=data[0], **self._style["body"])
+                if i % 2 == 1:
+                    style = self._style["body"]
+                else:
+                    style = self._style["body2"]
+                l1 = tk.Label(self, text=data[0], **style)
                 l1.grid(row=i, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
                 # print(self._data.get(data[1], data[1]), data[1])
-                l2 = tk.Label(self, text=self._data.get(data[1], data[1]), **self._style["body"])
+                l2 = tk.Label(self, text=self._data.get(data[1], data[1]), **style)
                 l2.grid(row=i, column=1, sticky=tk.N+tk.S+tk.E+tk.W)
 
-                l3 = tk.Label(self, text=data[2].strftime("%H:%M"), **self._style["body"])
+                l3 = tk.Label(self, text=data[2].strftime("%H:%M"), **style)
                 l3.grid(row=i, column=2, sticky=tk.N+tk.S+tk.E+tk.W)
 
-                l4 = tk.Label(self, text=data[3].strftime("%H:%M"), **self._style["body"])
+                l4 = tk.Label(self, text=data[3].strftime("%H:%M"), **style)
                 l4.grid(row=i, column=3, sticky=tk.N+tk.S+tk.E+tk.W)
-                l5 = tk.Label(self, text=data[4], **self._style["body"])
+                l5 = tk.Label(self, text=data[4], **style)
                 l5.grid(row=i, column=4, sticky=tk.N+tk.S+tk.E+tk.W)
 
-                l6 = tk.Label(self, text=data[5].title(), **self._style["body"])
+                l6 = tk.Label(self, text=data[5].title(), **style)
                 l6.grid(row=i, column=5, sticky=tk.N+tk.S+tk.E+tk.W)
 
                 i += 1
@@ -102,10 +109,10 @@ class HomeScreen(tk.Tk):
         
         self.__con = con
 
-        self.__logout_btn = tk.Label(self, text="DEPARTURES", font=("Adani Regular", 60, "bold"), fg="#ffffff", bg="#800000")
+        self.__logout_btn = tk.Label(self, text="DEPARTURES", **STYLE["title"])
         self.__logout_btn.grid(row=0, column=0, sticky=tk.E+tk.W)
         
-        self._timelbl = tk.Label(self, text="...", font=("Adani Regular", 35, "bold"), fg="#ffffff", bg="#800000")
+        self._timelbl = tk.Label(self, text="...", **STYLE["time"])
         self._timelbl.grid(row=0, column=1, sticky=tk.N+tk.S+tk.E+tk.W)
         
         with open('op.json', 'r') as f1:
