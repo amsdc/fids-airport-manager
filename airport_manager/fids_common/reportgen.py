c537e6d1b76d8e7121d3990282b9018267024287
Advaith Menon <83835839+advaithm582@users.noreply.github.com>
2022-11-26 07:51:53 +0530

    reliability factor

--- airport_manager/fids_common/reportgen.py
+++ airport_manager/fids_common/reportgen.py
@@ -18,7 +18,7 @@ class ReportPDF(FPDF):
         # Arial italic 8
         self.set_font('Arial', '', 8)
         # Page number
-        self.cell(0, 10, settings.getstring("airport", "iata")+ "/" + settings.getstring("airport", "icao") + ', page' + str(self.page_no()) + ' out of {nb}', 0, 0, 'R')
+        self.cell(0, 10, settings.getstring("airport", "iata")+ "/" + settings.getstring("airport", "icao") + ', page ' + str(self.page_no()) + ' out of {nb}', 0, 0, 'R')
 
     def colored_table(self, headings, rows, col_widths=(42, 39, 35, 42)):
         # Colors, line width and bold font:
@@ -37,7 +37,7 @@ class ReportPDF(FPDF):
         fill = False
         for row in rows:
             for i in range(len(row)):
-                self.cell(w=col_widths[i], h=6, txt=row[i], border="LR", align="L", fill=fill)
+                self.cell(w=col_widths[i], h=6, txt=str(row[i]), border="LR", align="L", fill=fill)
             self.ln()
             fill = not fill
         self.cell(sum(col_widths), 0, "", "T")
@@ -91,5 +91,55 @@ class ReportPDF(FPDF):
         #     self.ln(5)
         self.set_font('Arial', '', 10)
         self.colored_table(["InCode", "OutCode", "From", "To", "ETA", "ETD", "DelayDep (mins)", "DelayArr (mins)"], reslist, col_widths=(20, 20, 46, 46, 35, 35, 30, 30 ))
+        self.ln(10)
+
+        #cursor.execute("SELECT IF(`ifid` IS NULL, LEFT(`ofid`,2), LEFT(`ifid`, 2)), COUNT(*) AS 'noflt', COUNT(TIMESTAMPDIFF(MINUTE, `sta`, `eta`) > 0) AS 'countdelayarr', COUNT(TIMESTAMPDIFF(MINUTE, `sta`, `eta`) < 0) AS 'countearlyarr',  COUNT(TIMESTAMPDIFF(MINUTE, `sta`, `eta`) = 0) AS 'countontimearr', COUNT(TIMESTAMPDIFF(MINUTE, `std`, `etd`) > 0) AS 'countdelaydep', COUNT(TIMESTAMPDIFF(MINUTE, `std`, `etd`) < 0) AS 'countearlydep',  COUNT(TIMESTAMPDIFF(MINUTE, `std`, `etd`) = 0) AS 'countontimedep' FROM `flight` GROUP BY LEFT(`ifid`, 2), IF(`ifid` IS NULL, LEFT(`ofid`, 2), LEFT(`ifid`, 2)) ORDER BY LEFT(`ifid`, 2) ASC;")
+        
+        cursor.execute("""SELECT
+        IF(`ifid` IS NULL, LEFT(`ofid`, 2), LEFT(`ifid`, 2)) as 'iata',
+        COUNT(*) AS 'noflt',
+        SUM(TIMESTAMPDIFF(MINUTE, `sta`, `eta`) > 0) AS 'countdelayarr',
+        SUM(TIMESTAMPDIFF(MINUTE, `sta`, `eta`) < 0) AS 'countearlyarr',
+        SUM(IF(`ifid`, TIMESTAMPDIFF(MINUTE, `sta`, `eta`) = 0, 0)) AS 'countontimearr',
+        SUM(TIMESTAMPDIFF(MINUTE, `std`, `etd`) > 0) AS 'countdelaydep',
+        SUM(TIMESTAMPDIFF(MINUTE, `std`, `etd`) < 0) AS 'countearlydep',
+        SUM(IF(`ofid`, TIMESTAMPDIFF(MINUTE, `std`, `etd`) = 0, 0)) AS 'countontimedep'
+        FROM
+        `flight`
+        GROUP BY
+        IF(`ifid` IS NULL, LEFT(`ofid`, 2), LEFT(`ifid`, 2))
+        ORDER BY
+        LEFT(`ifid`, 2) ASC;
+        """)
+
+        lst2 = []
+        for res in cursor.fetchall():
+            iata = res[0]
+            # noflt = float(res[1] or 0)
+            countdelayarr = float(res[2] or 0)
+            countearlyarr = float(res[3] or 0)
+            countontimearr = float(res[4] or 0)
+            countdelaydep = float(res[5] or 0)
+            countearlydep = float(res[6] or 0)
+            countontimedep = float(res[7] or 0)
+            
+            noflt = sum((countdelayarr, countearlyarr, countontimearr, countearlydep, countdelaydep, countontimedep))
+            
+            relfac = (((-1)*((countdelayarr+countdelaydep)/noflt))
+                      + (countearlyarr+countearlydep)/noflt)
+            
+            lst2.append([iata, "{:.0f}".format(noflt), "{:.0f}".format(countdelayarr), "{:.0f}".format(countearlyarr), "{:.0f}".format(countontimearr), "{:.0f}".format(countdelaydep), "{:.0f}".format(countearlydep), "{:.0f}".format(countontimedep), "{:.4f}".format(relfac)])
+
+        self.set_font('Arial', 'BU', 25)
+        self.cell(0, 20, 'Airline-wise Consolidated Delay Report', align='L')
+        self.ln(20)
+        
+        self.set_font('Arial', '', 10)
+        self.colored_table(["IATACode", "#flt", "#arrdelay", "#arrearly", "#arrontime", "#depdelay", "#depearly", "#depontime", "R_f"], lst2, col_widths=(29,)*9)
+        
+        self.ln(10)
+        self.set_font('Arial', 'I', 12)
+        self.multi_cell(0, txt="NOTE: Reliability factor is the expectation of X, where X is a random variable between -1, 0 and 1. -1 deontes delayed flights, 0 on time, 1 early flights.")
+
         self.output(outfile)
 
